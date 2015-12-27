10c44cd7ba585569cd22f64a834af488c19822ac
cpachomski <cpachomski@gmail.com>
2015-12-27 10:28:47 -0500

    include router into app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
-document.write("Hello Squirrel");
+import Router from './router'
+import styles from './styles/main.styl'
+
+window.app = {
+  init () {
+    this.router = new Router()
+    this.router.history.start()
+  }
+}
 
 
+window.app.init()
