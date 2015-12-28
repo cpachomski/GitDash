24d9023e9657e9484b4003725e603849b5543f62
cpachomski <cpachomski@gmail.com>
2015-12-28 11:43:54 -0500

    create public and repos components

---
+++ src/pages/public.js
@@ -0,0 +1,18 @@
+weimport React from 'react'
+
+export default React.createClass({
+  render () {
+    <div className='container'>
+      <header role='banner'>
+        <h1>Labelr</h1>
+      </header>
+      <div>
+        <p>We label stuff for you, because, we can&trade;</p>
+        <a href='/login' className='button button-large'>
+          <span className='mega-octicon octicon-mark-github'></span> Login with GitHub
+        </a>
+      </div>
+    </div>
+  }
+})
+
---
+++ src/pages/repos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default React.createClass({
+  render () {
+    <div>
+      <h1>Repos </h1>
+    </div>
+  }
+})
+
