7f7e7097eaa834de5ab0a3983535ae82a5caa6b3
cpachomski <cpachomski@gmail.com>
2015-12-28 14:49:01 -0500

    successfully get callback auth code

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Router from 'ampersand-router'
 import React from 'react'
 import qs from 'qs'
+import xhr from 'xhr'
 import PublicPage from './pages/public'
 import ReposPage from './pages/repos'
 import Layout from './layout'
@@ -33,7 +34,8 @@ export default Router.extend({
   routes: {
     '': 'public',
     'repos': 'repos',
-    'login': 'login'
+    'login': 'login',
+    'auth/callback?:query' : 'authCallback'
   },
 
   public () {
@@ -44,13 +46,24 @@ export default Router.extend({
     this.renderPage(<ReposPage/>);
   },
 
-  login() {
+  login () {
     window.location = 'https://github.com/login/oauth/authorize?' + qs.stringify({
       client_id: 'c8648178b298b4cc1db0',
       redirect_uri: window.location.origin + '/auth/callback',
       scope: 'user, repo',
 
     })
+  },
+
+  authCallback (query) {
+    query = qs.parse(query);
+    console.log(query);
+    xhr({
+      url: 'https://thawing-plains-4936.herokuapp.com/authenticate/' + query.code,
+      json: true
+    }, (err, req, body) => {
+      console.log(body);
+    });
   }
 
 });
