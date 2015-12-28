52f91218982b798cba45dbaac5cd9cc3929840be
cpachomski <cpachomski@gmail.com>
2015-12-28 13:26:02 -0500

    create linker component

---
+++ src/helpers/linker.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import localLinks from 'local-links'
+
+
+export default React.createClass({
+  displayName: "Linker",
+
+
+  onClick (event) {
+    const pathname = localLinks.getLocalPathname(event);
+
+    if (pathname) {
+      event.preventDefault();
+      app.router.history.navigate(pathname);
+    }
+  },
+
+  render () {
+
+    return (
+      <div {...this.props} onClick={this.onClick}>
+        {this.props.children}
+      </div>
+    )
+  }
+
+})
