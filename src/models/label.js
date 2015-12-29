ade7862ebefeea885f294628f7619fceb08d5d70
cpachomski <cpachomski@gmail.com>
2015-12-29 13:14:35 -0500

    add unique id to lable model

--- src/models/label.js
+++ src/models/label.js
@@ -2,6 +2,8 @@ import Model from 'ampersand-model'
 import githubMixin from '../helpers/github-mixin'
 
 export default Model.extend({
+  idAttribute: 'name',
+
   props: {
     name: 'string',
     color: 'string'
