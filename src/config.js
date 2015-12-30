0c0901ce00a988923a14c5849ed227bf132e9712
cpachomski <cpachomski@gmail.com>
2015-12-29 20:43:06 -0500

    fix config and update package.json with new project name

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config = {
   'localhost': {
-    authUrl: 'https://thawing-plains-4936.herokuapp.com/authenticate'.
+    authUrl: 'https://thawing-plains-4936.herokuapp.com/authenticate',
     clientId: 'c8648178b298b4cc1db0'
 
   },
 
   'trub.surge.sh': {
-    authUrl: 'https://trub.herokuapp.com/authenticate'.
+    authUrl: 'https://trub.herokuapp.com/authenticate',
     clientId: '1560b89757565846fec7'
   },
 }[location.hostname]
