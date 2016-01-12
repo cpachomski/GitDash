669de2f39ebbca45231685b7ca0a93da0f061b32
cpachomski <cpachomski@gmail.com>
2016-01-12 01:20:45 -0500

    add utf-8 and try to decode readme content

--- src/templates/readme-preview.js
+++ src/templates/readme-preview.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ampersandMixin from 'ampersand-react-mixin'
+import base64 from 'base-64'
 
 
 export default React.createClass({
@@ -9,6 +10,7 @@ export default React.createClass({
   render () {
 
     const {readme} = this.props;
+    readme.content = base64.decode(readme.content);
 
 
     return (
