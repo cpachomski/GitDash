22d92b5a672458538f7539d9da5f83ef64b7ce78
cpachomski <cpachomski@gmail.com>
2016-01-13 02:19:45 -0500

    add window resize lifecycle methods. need to fix

--- src/pages/repo-detail.js
+++ src/pages/repo-detail.js
@@ -17,12 +17,35 @@ export default React.createClass({
     }, {at: 0})
   },
 
+  getInitialState () {
+    return {
+      componentHeight: window.innerheight
+    }
+  },
+
+  handleResize () {
+    this.setState({
+      componentHeight: window.innerheight
+    })
+  },
+
+  componentDidMount () {
+    window.addEventListener('resize', this.handleResize);
+  },
+
+  componentWillUnmount () {
+    window.removeEventListener('resize', this.handleResize);
+  },
+
   render () {
     const {repo, labels, readme} = this.props
+    let componentStyling = {
+      height: '' + this.state.componentHeight
+    }
 
     window.readme = readme;
     return (
-      <div className='container'>
+      <div className='repo-detail' style={componentStyling} >
         <h1>{repo.full_name}</h1>
         <ReadmePreview readme={readme} />
         <p>
